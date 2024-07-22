6ba6286aed8d7e4a26c5a65042c07fa1b3537726
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-07-22 15:02:27 +0200

    Reoganize factory code

---
+++ src/deployFactories.spec.ts
@@ -0,0 +1,65 @@
+import { expect } from "chai";
+import hre from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+
+import deployFactories from "./deployFactories";
+
+import {
+  address as singletonFactoryAddress,
+  fundingTransaction as singletonFundingTx,
+  signedDeployTransaction as singletonDeployRawTx,
+} from "./factories/singletonFactory";
+import { address as moduleFactoryAddress } from "./factories/moduleFactory";
+
+function reset() {}
+
+describe("deployFactories", () => {
+  it("Deploys both factories if none exists", async () => {
+    await loadFixture(reset as any);
+    const provider = hre.ethers.provider;
+    expect(await provider.getCode(singletonFactoryAddress)).to.equal("0x");
+    expect(await provider.getCode(moduleFactoryAddress)).to.equal("0x");
+
+    await deployFactories(hre);
+
+    expect(await provider.getCode(singletonFactoryAddress)).to.not.equal("0x");
+    expect(await provider.getCode(moduleFactoryAddress)).to.not.equal("0x");
+  });
+
+  it("Deploys the ModuleFactory is only the SingletonFactory exists", async () => {
+    await loadFixture(reset as any);
+    const [signer] = await hre.ethers.getSigners();
+
+    await (await signer.sendTransaction(singletonFundingTx)).wait();
+    await signer.provider.send("eth_sendRawTransaction", [
+      singletonDeployRawTx,
+    ]);
+
+    expect(await signer.provider.getCode(singletonFactoryAddress)).to.not.equal(
+      "0x"
+    );
+    expect(await signer.provider.getCode(moduleFactoryAddress)).to.equal("0x");
+
+    await deployFactories(hre);
+
+    expect(await signer.provider.getCode(singletonFactoryAddress)).to.not.equal(
+      "0x"
+    );
+    expect(await signer.provider.getCode(moduleFactoryAddress)).to.not.equal(
+      "0x"
+    );
+  });
+
+  it("Does nothing is both factories exist", async () => {
+    await loadFixture(reset as any);
+    const provider = hre.ethers.provider;
+    expect(await provider.getCode(singletonFactoryAddress)).to.equal("0x");
+    expect(await provider.getCode(moduleFactoryAddress)).to.equal("0x");
+
+    await deployFactories(hre);
+    await deployFactories(hre);
+
+    expect(await provider.getCode(singletonFactoryAddress)).to.not.equal("0x");
+    expect(await provider.getCode(moduleFactoryAddress)).to.not.equal("0x");
+  });
+});
---
+++ src/deployFactories.ts
@@ -0,0 +1,83 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+import {
+  address as singletonFactoryAddress,
+  signedDeployTransaction as deploySingletonFactorySignedTx,
+  fundingTransaction as singletonFundingTx,
+} from "./factories/singletonFactory";
+
+import {
+  address as moduleFactoryAddress,
+  deployTransaction as deployModuleFactoryTx,
+} from "./factories/moduleFactory";
+
+/**
+ * Get the singleton factory contract (ERC-2470).
+ * If it is not deployed on the network, then also deploy it.
+ *
+ * https://eips.ethereum.org/EIPS/eip-2470
+ * @param hardhat
+ * @returns Singleton Factory contract
+ */
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  const [signer] = await hre.ethers.getSigners();
+  {
+    const code = await signer.provider.getCode(singletonFactoryAddress);
+    if (code == "0x") {
+      await deploySingletonFactory(hre);
+    }
+  }
+
+  {
+    const code = await signer.provider.getCode(moduleFactoryAddress);
+    if (code == "0x") {
+      await deployModuleFactory(hre);
+    }
+  }
+}
+
+async function deploySingletonFactory(hre: HardhatRuntimeEnvironment) {
+  const [signer] = await hre.ethers.getSigners();
+
+  const receipt = await signer.sendTransaction(singletonFundingTx);
+  await receipt.wait();
+
+  const hash = await signer.provider.send("eth_sendRawTransaction", [
+    deploySingletonFactorySignedTx,
+  ]);
+  await waitForReceipt(hre, hash);
+
+  const code = await signer.provider.getCode(singletonFactoryAddress);
+  if (code == "0x") {
+    throw new Error("Bytecode for SingletonFactory not found");
+  }
+}
+
+async function deployModuleFactory(hre: HardhatRuntimeEnvironment) {
+  const [signer] = await hre.ethers.getSigners();
+
+  const receipt = await signer.sendTransaction(deployModuleFactoryTx);
+  await receipt.wait();
+
+  const code = await signer.provider.getCode(moduleFactoryAddress);
+  if (code == "0x") {
+    throw new Error("Bytecode for ModuleFactory not found");
+  }
+}
+
+async function waitForReceipt(hre: HardhatRuntimeEnvironment, hash: string) {
+  let receipt;
+  while (!receipt) {
+    receipt = await hre.ethers.provider.getTransactionReceipt(hash);
+    if (!receipt) {
+      await wait(200);
+    }
+  }
+}
+
+function wait(milliseconds: number): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
