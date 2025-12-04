04b38c8adfb693cf8a4845f8258aa43bffa90270
Cristóvão <cristovaoth@users.noreply.github.com>
2025-12-04 14:18:38 +0100

    Bump build dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,18 +1,8 @@
 import "@nomicfoundation/hardhat-toolbox";
 
 import dotenv from "dotenv";
-import yargs from "yargs";
 
 import type { HttpNetworkUserConfig } from "hardhat/types";
-
-const { network } = yargs
-  .option("network", {
-    type: "string",
-    default: "hardhat",
-  })
-  .help(false)
-  .version(false)
-  .parseSync();
 
 // Load environment variables.
 dotenv.config();
@@ -29,15 +19,6 @@ if (PK) {
   sharedNetworkConfig.accounts = {
     mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
   };
-}
-
-if (
-  ["mainnet", "goerli", "sepolia", "ropsten"].includes(network) &&
-  INFURA_KEY === undefined
-) {
-  throw new Error(
-    `Could not find Infura key in env, unable to connect to network ${network}`
-  );
 }
 
 export default {
